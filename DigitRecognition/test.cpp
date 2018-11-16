14495f27455924c70e37231ece363048878d9baf
Brett Dong <brett.browning.dong@gmail.com>
2018-11-16 22:28:25 +0800

    Add test case

---
+++ DigitRecognition/test.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "HandwrittenRecognizer.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    HandwrittenRecognizer recognizer;
+    cv::Mat image;
+    if(argc == 1)
+        image = cv::imread("test.jpg");
+    else
+        image = cv::imread(argv[1]);
+    cout << recognizer.recognize(image) << endl;
+    return 0;
+}
